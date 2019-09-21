b7fb12dc699e920cc4c1671528119e38efc8b3fa
Elin Ljunggren <elin.l.ljunggren@gmail.com>
2019-09-21 19:06:43 +0200

    Remove React app default html and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Link, Paragraph } from 'evergreen-ui'
-import logo from './img/logo.svg';
 import './App.css';
 import { Header } from './Header.js';
 
@@ -9,21 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Paragraph size={500}>
-            Edit <code>src/App.js</code> and save to reload.
-          </Paragraph>
-          <Link
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            size={500}
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Link>
-        </header>
       </div>
     );
   }
