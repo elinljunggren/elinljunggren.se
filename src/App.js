cad321a9b536ddb6d169d879d3df954257b143c2
Elin Ljunggren <elin.l.ljunggren@gmail.com>
2019-09-21 22:27:12 +0200

    First intro section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import me from './img/me-bw.jpg';
 import './App.css';
 import { Header } from './Header.js';
 
@@ -7,6 +8,23 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <section id="intro-section">
+          <div className="container">
+            <div className="flex-box">
+              <img src={me} className="img-me" />
+              <div id="intro-text">
+                <h2>I'm Elin,</h2>
+                <p>
+                  a Software Engineer interested in all things related to sustainability.
+                  Currently trying to make an impact through my lifestyle, my work
+                  at <a href="https://1928diagnostics.com" target="_blank">1928 Diagnostics</a> and
+                  activism together with <a href="https://extinctionrebellion.se" target="_blank">Extinction Rebellion</a>.
+                </p>
+                <p>This site is about me.</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
